7af0c6e01a48117d3a371ab6d7343c90b687de81
FKaag <francois.kaag@cardynal.fr>
2025-01-22 20:17:42 +0100

    Support several public keys
    And fix time claims

--- EVC generator/EVC_generator.py
+++ EVC generator/EVC_generator.py
@@ -4,70 +4,53 @@ import tkinter
 from pathlib import Path
 from rdflib import *
 from base45 import b45decode,b45encode
-from cwt import Claims,COSEKey
+from base64 import b64encode
+from cwt import Claims,COSEKey,CWT,load_pem_hcert_dsc
+from typing import Any,Union,Dict
+
+class MyCWT(CWT):
+    # This is to avoid the optional claims forced by PyCwt: iat,nbf et exp
+    def _set_default_value(self, claims: Union[Dict[int, Any], bytes]):
+        return
+
+mycwt = MyCWT()
 
 default = {
     "resourceType": "Bundle",
     "type": "collection",
-    "entry": [{
-			"fullUrl": "http://EVC/Patient/this",
+    "entry": [{"fullUrl": "http://EVC/Patient/this",
             "resource": {
 				"text": {"status": "generated", "div":"<div xmlns='http://www.w3.org/1999/xhtml'>Patient John DOË</div>"},
 				"id":"this",
                 "resourceType": "Patient",
-                "name": [{
-                        "family": "DOË",
-                        "given": ["John"]
-                    } ],               
+                "name": [{"family": "DOË","given": ["John"] } ],               
                 "birthDate": "2017-07-19"
-            }
-        },        
-       {
-		   "fullUrl": "http://EVC/Immunization/1",
+            }},        
+       {    "fullUrl": "http://EVC/Immunization/1",
             "resource": {
 				"text": {"status":"generated","div":"<div xmlns='http://www.w3.org/1999/xhtml'>REPEVAX administered on 2021-05-05</div>"},
-				"id":"1",
-                "resourceType": "Immunization",
-                "identifier": [{
-                        "system": "http://EVC/MasterRecord",
-                        "value": "FRA/36/2021-05-05/1245"
-                    } ],
+				"id":"1","resourceType": "Immunization",
+                "identifier": [{"system": "http://EVC/MasterRecord","value": "FRA/36/2021-05-05/1245"} ],
                 "status": "completed",
                 "vaccineCode": {
-                    "coding": [{
-                            "system": "urn:oid:1.3.6.1.4.1.48601.1.1.1",
-                            "code": "VAC0029",
-                            "display": "REPEVAX"
-                        }] },
+                    "coding": [{"system": "urn:oid:1.3.6.1.4.1.48601.1.1.1","code": "VAC0029","display": "REPEVAX"}] },
                 "patient": {"reference": "Patient/this"},
                 "occurrenceDateTime": "2021-05-05"				
-            }
-        }, 
-       {
-			"fullUrl": "http://EVC/Immunization/2",
+            }}, 
+       {    "fullUrl": "http://EVC/Immunization/2",
             "resource": {
 				"text": {"status":"generated","div":"<div  xmlns='http://www.w3.org/1999/xhtml'>TETRACOQ administered on 2022-03-03</div>"},
-				"id":"2",
-                "resourceType": "Immunization",
-                "identifier": [{
-                        "system": "http://EVC/MasterRecord",
-                        "value": "FRA/36/2022-03-03/127"
-                    }],
+				"id":"2","resourceType": "Immunization",
+                "identifier": [{"system": "http://EVC/MasterRecord","value": "FRA/36/2022-03-03/127"}],
                 "status": "completed",
                 "vaccineCode": {
-                    "coding": [{
-                            "system": "urn:oid:1.3.6.1.4.1.48601.1.1.1",
-                            "code": "VAC0063",
-                            "display": "TETRACOQ"
+                    "coding": [{"system": "urn:oid:1.3.6.1.4.1.48601.1.1.1","code": "VAC0063","display": "TETRACOQ"
                         } ] },
                 "patient": {"reference": "Patient/this" },
                 "occurrenceDateTime": "2022-03-03"				
-            }
-        }
-    ]
-}
+            }}]}
 
-# Données de signature
+# Données de signature SYA
 jwk = {
             "kty": "EC",
             "crv": "P-256",
@@ -76,9 +59,37 @@ jwk = {
             "y": "YPfxp4DYp4O/t6LdayeW6BKNu87509Fo25Uplxo257k",
             "d": "bBOCdlrsU1jxF3M9KBwce9w5iE0EpFoebGfIWLwgbBk",
             "kid": "AsymmetricECDSA256"
-          }		
+          }
+# Clé publique LUX (2024)
+pemlux = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAES7+ibOk3VEzhFhAH55tNgRDlNQBD\nB3pr97KtSis5dDIojsuHCgp8trQSHAqDs2LlGUvPQGKedNzmbirxCPS4jA==\n-----END PUBLIC KEY-----"
+# Ancienne clé LUX (2021)
+certlux="""-----BEGIN CERTIFICATE-----
+MIIE7jCCAqKgAwIBAgIIFqh8swSpPNEwQQYJKoZIhvcNAQEKMDSgDzANBglghkgBZQMEAgEFAKEcMBoGCSqGSIb3DQEBCDANBglghkgBZQMEAgEFAKIDAgEgMFoxCzAJ
+BgNVBAYTAkxVMR0wGwYDVQQKDBRJTkNFUlQgcHVibGljIGFnZW5jeTEsMCoGA1UEAwwjR3JhbmQgRHVjaHkgb2YgTHV4ZW1ib3VyZyBDU0NBIFRFU1QwHhcNMjExMTEw
+MTY0MzA3WhcNMzIwMTA3MTY0MzA2WjBcMQswCQYDVQQGEwJMVTEbMBkGA1UECgwSTWluaXN0cnkgb2YgSGVhbHRoMTAwLgYDVQQDDCdHcmFuZCBEdWNoeSBvZiBMdXhl
+bWJvdXJnIERTIENWRSBURVNUIDEwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAAQkNgSXxqTzujne5q8JdBc7Q0fK6HNH1aU1Ap+GRCfK/FvjLIWjcwwIqy4Bb9FuZv/9
+sID9Jvgj8dl4NR83cr7No4IBFzCCARMwHwYDVR0jBBgwFoAUm6ZBIYGn0NWe0pxQwMYre7jzGJowKwYDVR0SBCQwIoEOY3NjYUBpbmNlcnQubHWkEDAOMQwwCgYDVQQH
+DANMVVgwKwYDVR0RBCQwIoEOY3NjYUBpbmNlcnQubHWkEDAOMQwwCgYDVQQHDANMVVgwOgYDVR0fBDMwMTAvoC2gK4YpaHR0cDovL3JlcG9zaXRvcnkuaW5jZXJ0Lmx1
+L2NzY2EtdGVzdC5jcmwwHQYDVR0OBBYEFCPRmJzoLvJimSuXf7jhxDziEKvJMCsGA1UdEAQkMCKADzIwMjExMTEwMTY0MzA3WoEPMjAyMjA1MDkxNjQzMDdaMA4GA1Ud
+DwEB/wQEAwIHgDBBBgkqhkiG9w0BAQowNKAPMA0GCWCGSAFlAwQCAQUAoRwwGgYJKoZIhvcNAQEIMA0GCWCGSAFlAwQCAQUAogMCASADggIBAEmhyrdp5/qHq1cjTkgi
+agJPeirJOEqzZ7hv/FXMqrWyuzUl2Nzobim55t4qeThXdu0Ou94g2X0RKBVkIPOo9fEcroUp/bRmKydreAXMZxDzbLKJ/rdDiV8dNv3stB0jUOChZOO2nBWoHwrPh4t3
+XYYcEnSin3/y+6e1oDdgJV8qFu5o7hBwD6zCiIMKo5dm5toGSjD8U4D5vQy0UPlIK1SnKFcFZNhZbSqacTzqPOwEiAhaXie/WcskUY1kc31nGdgUKZ6eFZaXArfDxgP9
+dzfAxf8qdAEt5Dax2E6t+n6k2gPEqFV5WS7xrMDGvLt1uqgQ3X0bfR3OI6ghgVp0FBcZ/eZMXFlmaiPqkSmlqf+ffspjsxrXTmzEOP4V671MkOFojHEUxJy/kg6g7SKx
+AyUd7MaV2N37uqDHYQHzR8KQCJ9pcRTKOrON8VfSRQSKb1DoKghi4OuwLU8v0L1Q+9tPLOOX9L/7j+yRzNvJGOn+BqlDGKXrYXbN2iFA0zHw60WP1gkbWIqJrg+hYuBo
+lfy6cpukbJR+QtS/3jK4UxuSZBVjuy4+vudbCHzh/wAUdjjCEUklkc3TGEZWNebTWBVpFlo37XgETG8fapqbwkUnvrno6Sd38sDLfyuPogkV385qVvALfEkd0UmBfYF2
+a+kgba0kkt+zMD/4Ja+BF2q4
+-----END CERTIFICATE-----"""
 
 key =COSEKey.from_jwk(jwk)
+
+pubkeys=[]
+pubkeys.append(key)
+pubkey = load_pem_hcert_dsc(certlux)
+pubkeys.append(pubkey)
+pubkey = COSEKey.from_pem(pemlux,kid=b'\xc9\xd86y3\xb4\xecL')
+pubkeys.append(pubkey)
+
+keyexp = int(datetime.datetime.strptime("2050-12-31","%Y-%m-%d").timestamp())
 
 def doClear():
     source.delete('1.0',tkinter.END)
@@ -203,11 +214,10 @@ def doPack():
         result.insert('1.0',"Invalid JSON format")
         return
     
-    today= int(datetime.datetime.timestamp(datetime.datetime.now()))
+    today= int(datetime.datetime.timestamp(datetime.datetime.now()))    
+    topack = {"iss":"SYA","exp":keyexp, "iat":today, "hcert": sdata}
 
-    topack = {"iss":"SYA","iat":today,"hcert": sdata}
-
-    cose = cwt.encode(topack,key)
+    cose = mycwt.encode(topack,key)
     compressed = zlib.compress(cose)
     encoded = b45encode(compressed)
 
@@ -222,7 +232,7 @@ def doUnpack():
     try:
         compressed = b45decode(sresult)
         cose=zlib.decompress(compressed)
-        decoded=cwt.decode(cose,key)
+        decoded=cwt.decode(cose,pubkeys)
         claims=Claims.new(decoded)
     except:
         shrinked.insert("1.0","Invalid EVC format")
@@ -236,7 +246,6 @@ g = Graph()
 g.parse("https://ivci.org/nuva/nuva_core.ttl",format="turtle")
 
 #Build window and fields
-
 window=tkinter.Tk()
 
 frame1 = tkinter.Frame()
